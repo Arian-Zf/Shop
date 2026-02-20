24d2dbf98465ed09bd58f99198edfa5c1bf5c226
Arian <arianzafari2019@gmail.com>
2026-02-21 00:59:59 +0330

    just add timezone to accoutn

--- shop_main/account/models.py
+++ shop_main/account/models.py
@@ -1,4 +1,7 @@
+
+
 from django.db import models
+from django.utils import timezone
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
@@ -30,6 +33,7 @@ class ShopUser(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
     address = models.TextField()
 
     objects = ShopUserManeger()
