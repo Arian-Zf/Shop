988d69439ae2931fa470e9629528d9a7802fe5cb
Arian <arianzafari2019@gmail.com>
2026-02-20 20:35:44 +0330

    admin->ShopUser

--- shop_main/account/admin.py
+++ shop_main/account/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin  
+from .models import ShopUser
 
-# Register your models here.
+@admin.register(ShopUser)
+class ShopUserAdmin(UserAdmin):  
+    ordering = ['phone']
+    list_display = ('phone', 'first_name', 'last_name', 'is_active', 'is_staff')
+    
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('address',)}),
+    ) 
