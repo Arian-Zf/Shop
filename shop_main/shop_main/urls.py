c7dd17f60cdd0b2bac592f43c3d4f10430258e47
Arian-Zf <arianzafari2019@gmail.com>
2026-03-10 23:01:40 +0330

    add url veriFycode & veriFyform

--- shop_main/shop_main/urls.py
+++ shop_main/shop_main/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('order/', include('cart.orders', namespace='orders')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
