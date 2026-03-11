7e5ede2096e58c345c0915261ec3e0c8733f02c4
Arian-Zf <arianzafari2019@gmail.com>
2026-03-11 18:46:41 +0330

    step 2 verify_phone

--- shop_main/orders/views.py
+++ shop_main/orders/views.py
@@ -4,13 +4,30 @@ from .forms import PhoneVerificationForm
 from  account.models import ShopUser
 import random
 from django.contrib.auth import login
+from cart.common.KaveSms import send_sms_with_template
 
 
 
-def verify_form(request):
+def verify_phone(request):
     if request.method == 'POST':
-        pass
+        form = PhoneVerificationForm(request.POST)
+        if form.is_valid():
+            phone = form.cleaned_data['phone']
+            if ShopUser.objects.filter(phone=phone).exists():
+                messages.error(request, 'this phone is already registered.')
+                return redirect('orders:verify_phone')
+            else:
+                tokens = {'token': ''.join(random.choices('0123456789', k=6))}
+                request.session['verification_code'] = tokens['token']
+                request.phone = phone
+                send_sms_with_template(phone, tokens, 'verify')
+                messages.success(request, 'verificatio code send successfully ')
+                return redirect('orders:verify_code')
     else:
         form = PhoneVerificationForm()
 
-    return render(request, 'verify_form.html',{'form':form})
+    return render(request, 'verify_phone.html', {'form': form})
+
+
+def verify_code(request):
+    pass
