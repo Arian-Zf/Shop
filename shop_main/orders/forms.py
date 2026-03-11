dc33e07c94dceee6d18324d7b89da52a4a521858
Arian-Zf <arianzafari2019@gmail.com>
2026-03-11 17:05:04 +0330

    step1 of varifyform

--- shop_main/orders/forms.py
+++ shop_main/orders/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 
+
 class PhoneVerificationForm(forms.Form):
     phone = forms.CharField(max_length=11, label='شماره تلفن')
--- shop_main/orders/views.py
+++ shop_main/orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .forms import PhoneVerificationForm
+from  account.models import ShopUser
+import random
+from django.contrib.auth import login
 
-# Create your views here.
+
+
+def verify_form(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = PhoneVerificationForm()
+
+    return render(request, 'verify_form.html',{'form':form})
