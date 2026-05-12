81abd16c24716879fa7aefe6e157061055a3678c
Arian-Zf <arianzafari2019@gmail.com>
2026-05-13 01:01:30 +0330

    url

--- shop_main/api/urls.py
+++ shop_main/api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
         path('posts/', views.ProductListAPIView.as_view(), name='posts-list-api'),
         path('post/<int:pk>/', views.ProductDetailAPIView.as_view(), name='post-detail-api'),
         path('users/', views.UserListAPIViews.as_view(), name='user-list-api'),
+        path('register/', views.UserRegistrationAPIView.as_view(), name='register_api'),
 ]
