6890106b6dd0afcdf467c892011f8112c4be7edc
Arian-Zf <arianzafari2019@gmail.com>
2026-05-24 15:40:34 +0330

    url

--- shop_main/api/urls.py
+++ shop_main/api/urls.py
@@ -1,14 +1,17 @@
 from . import views
 from .views import *
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 app_name = 'api'
 
-from django.urls import path
+router = DefaultRouter()
+router.register(r'products', views.ProductViewSet)
 
 urlpatterns = [
-        path('posts/', views.ProductListAPIView.as_view(), name='posts-list-api'),
-        path('post/<int:pk>/', views.ProductDetailAPIView.as_view(), name='post-detail-api'),
+        # path('posts/', views.ProductListAPIView.as_view(), name='posts-list-api'),
+        # path('post/<int:pk>/', views.ProductDetailAPIView.as_view(), name='post-detail-api'),
         path('users/', views.UserListAPIViews.as_view(), name='user-list-api'),
         path('register/', views.UserRegistrationAPIView.as_view(), name='register_api'),
+        path('', include(router.urls)),
 ]
